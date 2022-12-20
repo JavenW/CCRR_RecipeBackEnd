d3bc1cf5613307c6d26b8da2c002a2ee54482f2c
Boxiong Kong <bk2808@columbia.edu>
2022-12-20 00:30:29 +0000

    Get Recipe by ingredients

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 import http.client
+import requests
 
 api = Flask(__name__)
 
@@ -21,3 +22,27 @@ def my_profile():
 
 
     return data.decode("utf-8")
+
+@app.route("/getrecipe", methods=["GET"])
+def get_recipe():
+    userid = request.args.get('userid', None)
+    print(userid)
+    url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients"
+
+    querystring = {"ingredients": "apples,flour,sugar", "number": "5", "ignorePantry": "true", "ranking": "1"}
+
+    headers = {
+        "X-RapidAPI-Key": "0f4d0b06f4mshe2ebd0d399fb0fcp1186e5jsne33884e5779d",
+        "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    result = response.json()
+
+    ret = []
+
+    for res in result:
+        ret.append(res['id'])
+
+    return ret
